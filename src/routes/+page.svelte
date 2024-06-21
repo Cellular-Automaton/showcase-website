14a80abd320e6938e63c2fab80da8f568d8bf4e4
Luka Camus <luka.camus@epitech.eu>
2024-06-21 15:38:52 +0200

    feat: init svelteKit project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>CAMI</title>
+</svelte:head>
+
+<section>
+	<h1>CAMI</h1>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
