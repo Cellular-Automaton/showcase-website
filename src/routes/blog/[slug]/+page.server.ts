ec1d37c1a25683b65791db353525eecfb2603d60
Luka Camus <luka.camus@hotmail.com>
2026-01-08 10:30:03 +0100

    feat(blog): prerender blog pages to be display in static app

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,11 +1,12 @@
 import type { PageServerLoad } from './$types';
 
+export const prerender = true;
 export const load: PageServerLoad = async ({ params }) => {
   const postFiles = import.meta.glob('/src/posts/blog/*.md');
   const postPath = `/src/posts/blog/${params.slug}.md`;
   const post = await postFiles[postPath]();
 
   return {
-    ...post.metadata,
+    ...post.metadata
   };
 };
