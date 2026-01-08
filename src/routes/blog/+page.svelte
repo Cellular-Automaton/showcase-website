4c1876bad9febc74240e86430d44f43d77dbf8b8
Luka Camus <luka.camus@hotmail.com>
2026-01-08 15:14:34 +0100

    feat(blog): warning for language unavailability

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -3,10 +3,17 @@
   import BlogCard from '$lib/components/BlogCard.svelte';
   import Timeline from 'flowbite-svelte/Timeline.svelte';
   import Heading from 'flowbite-svelte/Heading.svelte';
+  import Alert from 'flowbite-svelte/Alert.svelte';
+  import { ExclamationCircleOutline } from 'flowbite-svelte-icons';
+  import { m } from '$lib/paraglide/messages.js';
 </script>
 
-<div class="top-0 mt-4 flex flex-col gap-4">
+<div class="top-0 mt-5 flex flex-col gap-4">
   <Heading tag="h1" class="text-primary-700 dark:text-secondary-700">Blog</Heading>
+  <Alert class="mt-2 mb-3 flex flex-row justify-stretch p-5 shadow-lg">
+    {#snippet icon()}<ExclamationCircleOutline class="h-5 w-5" />{/snippet}
+    <p>{m.blog_lang_warning()}</p>
+  </Alert>
   <Timeline order="vertical">
     {#each data.posts as post}
       <BlogCard {post} />
